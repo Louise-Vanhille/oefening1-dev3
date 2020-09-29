6b70ebf839a8056c850823f5a560a3c4d8a77771
Louise Vanhille <louisevanhille@MilouMacBookPro.local>
2020-09-29 16:37:05 +0200

    Lifespan verlagen van particles

---
+++ Desktop/DEVINE/DEV/DEV3/02-OO/startproject-bronbestanden/oef01-a-totrandom/js/classes/Particle.js
@@ -0,0 +1,17 @@
+import Vector from './Vector.js';
+
+export default class Particle {
+  constructor(x, y) {
+	this.location = new Vector(x, y);
+	this.velocity = new Vector(0, 0);
+	this.acceleration = new Vector(0, 0.05);
+	this.lifespan = 100;
+  }
+  draw(ctx) {
+	this.velocity.add(this.acceleration);
+	this.location.add(this.velocity);
+	ctx.fillStyle = `rgba(255, 255, 255, ${this.lifespan / 100})`;
+	ctx.fillRect(this.location.x-1, this.location.y-1, 2, 2);
+	this.lifespan--;
+  }
+}
